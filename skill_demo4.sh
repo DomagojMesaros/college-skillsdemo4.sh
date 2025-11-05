188b30bbaad3a3ad6bd387e7495fe451c0b585c4
DomagojMesaros <mesarosdomagoj@protonmail.com>
2025-11-05 14:43:45 +0000

    Update skill_demo4.sh

--- skill_demo4.sh
+++ skill_demo4.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
+set -euo pipefail
+
 sudo apt install tree -y
+echo "/home/admin will be removed if it exists"
+echo "Removing folders"
 
+sudo rm -r /home/admin
 
-sudo mkdir /home/admin & sudo mkdir /home/admin/office & sudo mkdir /home/admin/accounts & sudo mkdir /home/admin/office/pending
+echo "Creating folders"
+sudo mkdir /home/admin
+sudo mkdir /home/admin/office
+sudo mkdir /home/admin/accounts 
+sudo mkdir /home/admin/office/pending
+
+echo "Folders Created"
+
+echo "Allowing sudoers access to /home/admin recursively"
+echo "Completed"
 
 sudo chgrp -R sudo /home/admin
 sudo chmod -R 775 /home/admin
+
+echo "Admin Privileges applied"
 
 sudo setfacl -m g:office:rwx /home/admin/office
 sudo setfacl -m g:accounts:rwx /home/admin/accounts
